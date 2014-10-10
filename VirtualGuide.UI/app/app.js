372f88932c18885ffe959d71a00a1b2bd5ebffea
Katarzyna Biernat <katarzyna@biernat.me>
2014-10-10 13:12:37 +0200

    Angular registration

---
+++ VirtualGuide.UI/app/app.js
@@ -0,0 +1,37 @@
+﻿var app = angular.module('VirtualGuide', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+app.config(function ($routeProvider) {
+
+    var site_prefix = '/VirtualGuide.UI';
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: site_prefix + "/app/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: site_prefix + "/app/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: site_prefix + "/app/views/signup.html"
+    });
+
+    $routeProvider.when("/travels", {
+        controller: "travelsController",
+        templateUrl: site_prefix + "/app/views/travels.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
+
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
