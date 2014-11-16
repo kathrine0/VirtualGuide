28c6abd276cc942c1cea271516d45aa997ecf44d
Katarzyna Biernat <katarzyna@biernat.me>
2014-11-16 23:10:31 +0100

    just remove commented code

--- VirtualGuide.Models/Models/User.cs
+++ VirtualGuide.Models/Models/User.cs
@@ -34,25 +34,6 @@ namespace VirtualGuide.Models
             return userIdentity;
         }
 
-        //public User(string firstname)
-        //    : base(firstname)
-        //{
-        //    PurchasedTravels = new List<User_Purchased_Travel>();
-        //    Travels = new List<Travel>();
-        //}
-
-        //[EmailAddressAttribute]
-        //public override string UserName
-        //{
-        //    get
-        //    {
-        //        return base.UserName;
-        //    }
-        //    set
-        //    {
-        //        base.UserName = value;
-        //    }
-        //}
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public virtual ICollection<User_Purchased_Travel> PurchasedTravels { get; set; }
