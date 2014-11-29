645fb4694c2038b61cd55cacfd9ee4824850b2cd
Katarzyna Biernat <katarzyna@biernat.me>
2014-11-29 15:33:52 +0100

    Loader indicators

---
+++ VirtualGuide.Mobile/VirtualGuide.Mobile.Shared/Converters/EmptyIsTrueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Windows.UI.Xaml.Data;
+
+namespace VirtualGuide.Mobile.Converters
+{
+    public class EmptyIsTrueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var collection = value as IEnumerable<object>;
+            
+            if (collection == null || collection.Count() == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VirtualGuide.Mobile/VirtualGuide.Mobile.Shared/Converters/NullIsTrueConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Data;
+
+namespace VirtualGuide.Mobile.Converters
+{
+    public class NullIsTrueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
